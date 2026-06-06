ca4e0f6a9e99163e01b3912947f9a166d81a1b1d
Jan <ireliayasuo.200@gmail.com>
2026-06-06 14:51:08 -0500

    Organizando estructura

--- industrial-maps/src/main.jsx
+++ industrial-maps/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.jsx";
+import App from "./App";
 import "./styles/global.css";
+
 ReactDOM.createRoot(document.getElementById("app")).render(
   React.createElement(
     React.StrictMode,
